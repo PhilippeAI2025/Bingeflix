1555af68b082d54eeffdc39235f302ae81203e98
FuturesProspect223 <77144077+PhilippeAI2025@users.noreply.github.com>
2026-05-19 14:14:50 +0200

    Fetch van API gefixt

--- Bingeflix/src/js/api.js
+++ Bingeflix/src/js/api.js
@@ -1,13 +1,13 @@
 // Eigen API key
-const apiKey = "c20886fe743a2f7fce2499542f90d3ec"
-const baseUrl = "https://www.themoviedb.org/"
+const apiKey = "c20886fe743a2f7fce2499542f90d3ec";
+const baseUrl = "https://api.themoviedb.org/3";
 
 // Populairste films op te halen
 export async function fetchPopularFilms() {
   try {
     const response = await fetch(`${baseUrl}/movie/popular?api_key=${apiKey}&language=nl-NL&page=1`);
     const data = await response.json();
-    return data.resuls.slice(0, 20); 
+    return data.results.slice(0, 20); 
   } catch (error) {
     console.error("Fout bij het ophalen van populaire films:", error);
     return []; 
@@ -19,7 +19,7 @@ export async function fetchFilmsSearchBar(term) {
   try {
     const response = await fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${encodeURIComponent(term)}&language=nl-NL&page=1`);
     const data = await response.json();
-    return data.resuls.slice(0, 20);
+    return data.results.slice(0, 20);
   } catch (error) {
     console.error("Fout bij het zoeken naar films:", error);
     return [];
@@ -31,7 +31,7 @@ export async function fetchFilmsOnGenre(genreId) {
   try {
     const response = await fetch(`${baseUrl}/discover/movie?api_key=${apiKey}&with_genres=${genreId}&language=nl-NL&page=1`);
     const data = await response.json();
-    return data.resltds.slice(0, 20);
+    return data.results.slice(0, 20);
   } catch (error) {
     console.error("Fout bij het ophalen van genre-films:", error);
     return [];
